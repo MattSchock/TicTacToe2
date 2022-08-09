75bd1221d88be605ac34e68b5e99d07136eb65c2
Matt <mattschock392@gmail.com>
2022-08-09 17:16:42 -0500

    stf

--- scripts.js
+++ scripts.js
@@ -3,6 +3,43 @@ const player2 = 'O';
 let heading = $("#heading");
 let currentPlayer = player1;
 
+let box1 = $('#box1');
+let box2 = $('#box2');
+let box3 = $('#box3');
+let box4 = $('#box4');
+let box5 = $('#box5');
+let box6 = $('#box6');
+let box7 = $('#box7');
+let box8 = $('#box8');
+let box9 = $('#box9');
+
+
+
+const boxIdArr = [
+    box1, box2, box3, box4, box5, box6, box7, box8, box9
+]
+
+console.log(boxIdArr);
+
+
+
+
+
+
+
+
+
+
+
+const winningCombos = [
+
+]
+
+
+
+
+
+
 
 
 
