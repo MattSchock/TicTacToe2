47cb46bf18b2e024859025ee1eacdbad40796be8
Matt <mattschock392@gmail.com>
2022-08-09 16:40:15 -0500

    stf

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const player1 = 'X';
 const player2 = 'O';
+let heading = $("#heading");
 let currentPlayer = player1;
 
 
@@ -12,11 +13,15 @@ function clickSelector(clicked_Id) {
     document.getElementById(boxId).innerHTML = currentPlayer;
     document.getElementById(boxId).removeAttribute("onClick");
     if (currentPlayer == player1) {
-        currentPlayer = player2
+        currentPlayer = player2;
+        heading.text('It is O turn.');
         return currentPlayer;
     } else {
         currentPlayer = player1;
+        heading.text('it is X turn');
         return currentPlayer;
     };
 };
+
+
 
