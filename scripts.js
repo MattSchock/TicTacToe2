e04b3c3ded386db493299dc51e098aab1902e478
Matt <mattschock392@gmail.com>
2022-08-12 09:55:51 -0500

    i have absolutely no idea why but its working

--- scripts.js
+++ scripts.js
@@ -1,136 +1,173 @@
-const player1 = 'X';
-const player2 = 'O';
-let heading = $("#heading");
-let currentPlayer = player1;
-let allDivs = $('.col-4')
+const player1 = 'X';     //assigns player1 as string x
+const player2 = 'O';     //assigns player 2 as string o
+let heading = $("#heading");      //jquery to target heading to change on turns
+let currentPlayer = player1;      //starts game as x player
+let allDivs = $('.col-4')         //jquery to target all squares. 
+let i0 = ''
+let i1 = ''
+let i2 = ''
 
+//jquery to get divs for boxes
+let box0 = $('#box0')[0].id;
+let box1 = $('#box1')[0].id;
+let box2 = $('#box2')[0].id;
+let box3 = $('#box3')[0].id;
+let box4 = $('#box4')[0].id;
+let box5 = $('#box5')[0].id;
+let box6 = $('#box6')[0].id;
+let box7 = $('#box7')[0].id;
+let box8 = $('#box8')[0].id;
 
-
-let box0 = $('#box0');
-let box1 = $('#box1');
-let box2 = $('#box2');
-let box3 = $('#box3');
-let box4 = $('#box4');
-let box5 = $('#box5');
-let box6 = $('#box6');
-let box7 = $('#box7');
-let box8 = $('#box8');
-
-
+//available moves left
 let moveOptions = ['box0', 'box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8'];
 
+
+//im pretty sure this currently doesnt do anything
 const boxIdArr = [
     box0, box1, box2, box3, box4, box5, box6, box7, box8
 ];
 
 // console.log(boxIdArr);
 
-// const winningCombos = [
-// [box0, box1, box2], 
-// [box3, box4, box5],  
-// [box6, box7, box8],  
-// [box0, box3, box6],  
-// [box1, box4, box7],  
-// [box2, box5, box8],  
-// [box0, box4, box8],
-// [box2, box4, box6]
-// ];
+
+//Win conditions
+const winningCombos = [
+[box0, box1, box2], 
+[box3, box4, box5],  
+[box6, box7, box8],  
+[box0, box3, box6],  
+[box1, box4, box7],  
+[box2, box5, box8],  
+[box0, box4, box8],
+[box2, box4, box6]
+];
 
 
+//arrays to hold player selections
 let xPlayerSelections = []
 let oPlayerSelections = []
 
-// function winChecker(x) {
-//     for(let i = 0; i < winningCombos.length; i++) {
-//         if(winningCombos[i].every(x => xPlayerSelections.includes(x)));
-//     }
-// }
 
 
+//Reset button. removes text X and O and replaces onclick listeners. also clears player selections and clears and replaces move options.
 function resetButton () {
-    allDivs.text(' ')
-    allDivs.attr('onClick', 'clickSelector(this.id)')
-    xPlayerSelections.splice(0, xPlayerSelections.length);
-    oPlayerSelections.splice(0, oPlayerSelections.length);
-    moveOptions.splice(0, moveOptions.length);
-    moveOptions.push('box0', 'box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8');
+    allDivs.text(' ')       //clears X and O
+    allDivs.attr('onClick', 'clickSelector(this.id)')   //replaces onclick listeners for squares
+    xPlayerSelections.splice(0, xPlayerSelections.length);    //clears x players choices array
+    oPlayerSelections.splice(0, oPlayerSelections.length);    //clears o players choices array
+    moveOptions.splice(0, moveOptions.length);                //clears move options array
+    moveOptions.push('box0', 'box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8');    //repopulates available move options array.
 }
 
 
 
-
+//experimental code below
 function clickSelector(clicked_Id) {
-    // console.log(clicked_Id);
     let boxId = clicked_Id;
-     console.log(boxId);
     document.getElementById(boxId).innerHTML = currentPlayer;
     document.getElementById(boxId).removeAttribute("onClick");
-    // console.log(document.getElementById(boxId).innerHTML);
-    for(let i =0; i < moveOptions.length; i++) {
+    for(let i = 0; i < moveOptions.length; i++) {
         if (moveOptions[i] == boxId) {
             moveOptions.splice(i, 1);
         }
     };
-    console.log("available moves", moveOptions);
-    
+
     if (currentPlayer == player1) {
         heading.text('It is O turn.');
         xPlayerSelections.push(boxId);
-        console.log('xPlayer', xPlayerSelections);
-        if(xPlayerSelections.includes('box0') && xPlayerSelections.includes('box1') && xPlayerSelections.includes('box2') 
-        || xPlayerSelections.includes('box3') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box5') 
-        || xPlayerSelections.includes('box6') && xPlayerSelections.includes('box7') && xPlayerSelections.includes('box8')
-        || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box3') && xPlayerSelections.includes('box6')
-        || xPlayerSelections.includes('box1') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box7')
-        || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box5') && xPlayerSelections.includes('box8')
-        || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box8')
-        || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box6')) {
-            allDivs.removeAttr("onClick");
-            heading.text("X Wins!!!!!!!")
-        } else if (moveOptions.length == 0) {
-            heading.text('Tie game')
+        console.log('xplayerselections', xPlayerSelections);
+        console.log('winning combos', winningCombos[0]);
+        console.log(xPlayerSelections, 'xplayerselections');
+        console.log(moveOptions, 'move options');
+        for( let i = 0; i < winningCombos.length; i++) {
+            console.log(winningCombos[i], winningCombos.length, 'Win conditions');
+            console.log('Testtesttest', typeof winningCombos[i][0]);
+            console.log('typeof xplayer', typeof xPlayerSelections[0]);
+            if(xPlayerSelections.includes(winningCombos[i][0]) && xPlayerSelections.includes(winningCombos[i][1]) && xPlayerSelections.includes(winningCombos[i][2])) {
+                alert('its working');
+            }
+            
+            }    
+                
+            
+            
         }
-        currentPlayer = player2
-        return currentPlayer;
-    } else {
-        heading.text('it is X turn');
-        oPlayerSelections.push(boxId);
-        console.log('oPlayer', oPlayerSelections);
-        if(xPlayerSelections.includes('box0') && xPlayerSelections.includes('box1') && xPlayerSelections.includes('box2') 
-        || xPlayerSelections.includes('box3') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box5') 
-        || xPlayerSelections.includes('box6') && xPlayerSelections.includes('box7') && xPlayerSelections.includes('box8')
-        || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box3') && xPlayerSelections.includes('box6')
-        || xPlayerSelections.includes('box1') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box7')
-        || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box5') && xPlayerSelections.includes('box8')
-        || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box8')
-        || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box6')) {
-            allDivs.removeAttr("onClick");
-            heading.text("O Wins!!!!!!!")
-        } else if (moveOptions.length == 0) {
-            heading.text('Tie game')
-        }
-        currentPlayer = player1;
-        return currentPlayer;
-
-    };
-};
-
-
-// if (currentPlayer == 'X') {
-//     xPlayerSelections.push(boxId);
-//     heading.text('Current Player is X')
-//     console.log(xPlayerSelections);
-//     return currentPlayer;
-// } else if (currentPlayer == 'O') {
-//     heading.text('Current Player is O')
-//     oPlayerSelections.push(boxId);
-//     return currentPlayer;
-
-// }
+    }
 
 
 
 
-// function checkForWinner() {
 
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Working code below
+// function clickSelector(clicked_Id) {
+//     // console.log(clicked_Id);
+//     let boxId = clicked_Id;
+//      console.log(boxId);
+//     document.getElementById(boxId).innerHTML = currentPlayer;
+//     document.getElementById(boxId).removeAttribute("onClick");
+//     // console.log(document.getElementById(boxId).innerHTML);
+//     for(let i =0; i < moveOptions.length; i++) {
+//         if (moveOptions[i] == boxId) {
+//             moveOptions.splice(i, 1);
+//         }
+//     };
+//     console.log("available moves", moveOptions);
+    
+//     if (currentPlayer == player1) {
+//         heading.text('It is O turn.');
+//         xPlayerSelections.push(boxId);
+//         console.log('xPlayer', xPlayerSelections);
+//         if(xPlayerSelections.includes('box0') && xPlayerSelections.includes('box1') && xPlayerSelections.includes('box2') 
+//         || xPlayerSelections.includes('box3') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box5') 
+//         || xPlayerSelections.includes('box6') && xPlayerSelections.includes('box7') && xPlayerSelections.includes('box8')
+//         || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box3') && xPlayerSelections.includes('box6')
+//         || xPlayerSelections.includes('box1') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box7')
+//         || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box5') && xPlayerSelections.includes('box8')
+//         || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box8')
+//         || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box6')) {
+//             allDivs.removeAttr("onClick");
+//             heading.text("X Wins!!!!!!!")
+//         } else if (moveOptions.length == 0) {
+//             heading.text('Tie game')
+//         }
+//         currentPlayer = player2
+//         return currentPlayer;
+//     } else {
+//         heading.text('it is X turn');
+//         oPlayerSelections.push(boxId);
+//         console.log('oPlayer', oPlayerSelections);
+//         if(xPlayerSelections.includes('box0') && xPlayerSelections.includes('box1') && xPlayerSelections.includes('box2') 
+//         || xPlayerSelections.includes('box3') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box5') 
+//         || xPlayerSelections.includes('box6') && xPlayerSelections.includes('box7') && xPlayerSelections.includes('box8')
+//         || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box3') && xPlayerSelections.includes('box6')
+//         || xPlayerSelections.includes('box1') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box7')
+//         || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box5') && xPlayerSelections.includes('box8')
+//         || xPlayerSelections.includes('box0') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box8')
+//         || xPlayerSelections.includes('box2') && xPlayerSelections.includes('box4') && xPlayerSelections.includes('box6')) {
+//             allDivs.removeAttr("onClick");
+//             heading.text("O Wins!!!!!!!")
+//         } else if (moveOptions.length == 0) {
+//             heading.text('Tie game')
+//         }
+//         currentPlayer = player1;
+//         return currentPlayer;
+
+//     };
+// };
+
+
